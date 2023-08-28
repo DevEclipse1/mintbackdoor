fbf5cd68501f7ba94d72d914071c793be742ca10
Kokonin <98752478+Kokonin@users.noreply.github.com>
2023-08-28 19:09:19 +0200

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("pip install discord.py")
+os.system("pip install pillow")
+os.system("pip install pynput")
+os.system("pip install gputil")
+os.system("pip install customtkinter")
